d217250ac2774ecc4c0cd63803f736953884147a
Duong <duongnartist@gmail.com>
2016-04-20 22:41:53 +0700

    add mongolite

---
+++ shiny/mongolite.R
@@ -0,0 +1,39 @@
+library(mongolite)
+
+options(mongodb = list(
+  "host" = "ds025180.mlab.com:25180",
+  "username" = "duongnartist",
+  "password" = "123123"
+))
+databaseName <- "real_estate"
+collectionName <- "informations"
+
+saveData <- function(data) {
+  # Connect to the database
+  db <- mongo(collection = collectionName,
+              url = sprintf(
+                "mongodb://%s:%s@%s/%s",
+                options()$mongodb$username,
+                options()$mongodb$password,
+                options()$mongodb$host,
+                databaseName))
+  # Insert the data into the mongo collection as a data.frame
+  data <- as.data.frame(t(data))
+  db$insert(data)
+}
+
+loadData <- function() {
+  # Connect to the database
+  db <- mongo(collection = collectionName,
+              url = sprintf(
+                "mongodb://%s:%s@%s/%s",
+                options()$mongodb$username,
+                options()$mongodb$password,
+                options()$mongodb$host,
+                databaseName))
+  # Read all the entries
+  data <- db$find()
+  data
+}
+categories <- data.frame(loadData())
+saveData(c(name = "Duong"))
